9742c5d852edc86b0c7b761d00ca4490d63e2215
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-04-10 14:13:45 +0200

    Lesson 24

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 
 
-def january(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Eat no chocolate for the entire month")
+challenges = {
+    'january': 'Eat no chocolate for the entire month',
+    'february': 'Walk for at least 20 minutes every day',
+    'march': 'Learn Django for at least 20 minutes every day'
+}
 
 
-def february(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Walk for at least 20 minutes every day")
+def monthly_challenge(request: HttpRequest, month: str) -> HttpResponse:
+    if month in challenges:
+        return HttpResponse(challenges[month])
+    return HttpResponseNotFound('This month is not supported')

40f151f000697b35d3bbf14ff66d26285827a233
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-04-10 14:07:14 +0200

    Lesson 23

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("This works!")
+def january(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Eat no chocolate for the entire month")
+
+
+def february(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Walk for at least 20 minutes every day")

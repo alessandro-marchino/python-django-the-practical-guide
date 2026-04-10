fc984eda8e9d4dba1128dcb2a5889ddc039c769c
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-04-10 14:21:20 +0200

    Lesson 26

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -5,14 +5,25 @@ from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 challenges = {
     'january': 'Eat no chocolate for the entire month',
     'february': 'Walk for at least 20 minutes every day',
-    'march': 'Learn Django for at least 20 minutes every day'
+    'march': 'Learn Django for at least 20 minutes every day',
+    'april': 'Eat no chocolate for the entire month',
+    'may': 'Walk for at least 20 minutes every day',
+    'june': 'Learn Django for at least 20 minutes every day',
+    'july': 'Eat no chocolate for the entire month',
+    'august': 'Walk for at least 20 minutes every day',
+    'september': 'Learn Django for at least 20 minutes every day',
+    'october': 'Eat no chocolate for the entire month',
+    'november': 'Walk for at least 20 minutes every day',
+    'december': 'Learn Django for at least 20 minutes every day'
 }
 
 
 def monthly_challenge(request: HttpRequest, month: str) -> HttpResponse:
-    if month in challenges:
+    try:
         return HttpResponse(challenges[month])
-    return HttpResponseNotFound('This month is not supported')
+    except:
+        return HttpResponseNotFound('This month is not supported')
+
 
 def monthly_challenge_by_number(request: HttpRequest, month: int) -> HttpResponse:
     return HttpResponse(month)

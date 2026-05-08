e933021300a7388a158ca4f5f3517bbff199785e
amarchino <marchino.alessandro@gmail.com>
2026-05-08 22:42:11 +0200

    Lesson 63

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -8,5 +8,5 @@ def starting_page(request: HttpRequest) -> HttpResponse:
 def posts(request: HttpRequest) -> HttpResponse:
     return render(request, 'blog/all-posts.html')
 
-def post_detail(request: HttpRequest) -> HttpResponse:
-    pass
+def post_detail(request: HttpRequest, slug: str) -> HttpResponse:
+    return render(request, 'blog/post-detail.html')

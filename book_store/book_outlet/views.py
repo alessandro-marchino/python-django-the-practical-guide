418360ebe00b065c103d3d6e6a2d4904d11555e3
amarchino <marchino.alessandro@gmail.com>
2026-05-10 20:44:44 +0200

    Lesson 89

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404
 
 from .models import Book
 
@@ -9,3 +9,7 @@ def index(req: HttpRequest) -> HttpResponse:
     return render(req, 'book_outlet/index.html', {
         "books": books
     })
+
+def book_detail(req: HttpRequest, id: int) -> HttpResponse:
+    book = get_object_or_404(Book, pk=id)
+    return render(req, 'book_outlet/book_detail.html', book.__dict__)

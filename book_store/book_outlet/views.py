1a57b313643ff1f309618fa81764b0d5de969ec3
amarchino <marchino.alessandro@gmail.com>
2026-05-10 21:07:43 +0200

    Lesson 93

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpRequest, Http404
+from django.http import HttpResponse, HttpRequest
+from django.db.models import Avg
 
 from .models import Book
 
 # Create your views here.
 def index(req: HttpRequest) -> HttpResponse:
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("title")
     return render(req, 'book_outlet/index.html', {
-        "books": books
+        "books": books,
+        "total_number_of_books": books.count(),
+        "average_rating": books.aggregate(Avg('rating'))
     })
 
 def book_detail(req: HttpRequest, slug: str) -> HttpResponse:

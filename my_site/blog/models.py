5ddc0d56727098689738bff3c8e66186d1a7273b
amarchino <marchino.alessandro@gmail.com>
2026-05-14 22:17:02 +0200

    Lesson 121

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -9,9 +15,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[ MinLengthValidator(10) ])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
 
-class Author(models.Model):
-    pass
 
 class Tag(models.Model):
     pass

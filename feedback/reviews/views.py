f9e7b22be26ec56c35d10616b0ed5a50cd9d16df
amarchino <marchino.alessandro@gmail.com>
2026-05-15 22:22:42 +0200

    Lesson 137

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 def review(req: HttpRequest) -> HttpResponse:
-    return render(req, "reviews/review.html")
+    if req.method == 'GET':
+      return render(req, "reviews/review.html")
+    entered_username = req.POST['username']
+    print(entered_username)
+    return HttpResponseRedirect('/thank-you')
+
+def thank_you(req: HttpRequest) -> HttpResponse:
+   return render(req, "reviews/thank-you.html")

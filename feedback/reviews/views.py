01d1629b008434606d80b2e00a237c2e3e8be23b
amarchino <marchino.alessandro@gmail.com>
2026-05-17 16:30:42 +0200

    Lesson 145

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 def review(req: HttpRequest) -> HttpResponse:
@@ -16,7 +17,8 @@ def review(req: HttpRequest) -> HttpResponse:
        return render(req, "reviews/review.html", {
           "form": form
        })
-    print(form.cleaned_data)
+    review = Review(user_name=form.cleaned_data["user_name"], review_text=form.cleaned_data["review_text"], rating=form.cleaned_data["rating"])
+    review.save()
     return HttpResponseRedirect('/thank-you')
 
 def thank_you(req: HttpRequest) -> HttpResponse:

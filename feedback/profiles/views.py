30eb630c6bb3858240bdd5a3f26bd7be453e4151
amarchino <marchino.alessandro@gmail.com>
2026-05-17 21:51:08 +0200

    Lesson 170

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,35 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.views import View
-from django.core.files.uploadedfile import UploadedFile
+from django.views.generic.edit import CreateView
 
-from .forms import ProfileForm
 from .models import UserProfile
 
 # Create your views here.
 
-def store_files(file: UploadedFile):
-    with open("temp/file.pdf", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
-
-
-
-class CreateProfileView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    def post(self, request: HttpRequest) -> HttpResponse:
-        form = ProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-          profile = UserProfile(image=request.FILES["user_image"])
-          profile.save()
-          return HttpResponseRedirect("/profiles")
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"

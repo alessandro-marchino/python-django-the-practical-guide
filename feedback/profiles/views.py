44041d3b3a0c9d1de20eee069faf33aad02b51a6
amarchino <marchino.alessandro@gmail.com>
2026-05-17 21:23:00 +0200

    Lesson 165

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.views import View
 
 # Create your views here.
 
 
 class CreateProfileView(View):
-    def get(self, request):
+    def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "profiles/create_profile.html")
 
-    def post(self, request):
-        pass
+    def post(self, request: HttpRequest) -> HttpResponse:
+        image = request.FILES["image"]
+        print(image)
+        return HttpResponseRedirect("/profiles")

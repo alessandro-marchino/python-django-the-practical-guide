b62d32b04df4da6d31928ea32dad1879b6189c4e
amarchino <marchino.alessandro@gmail.com>
2026-05-30 20:38:03 +0200

    Lesson 231

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -4,4 +4,11 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def index(req: HttpRequest) -> HttpResponse:
-    return render(req, 'meetups/index.html')
+    meetups = [
+        { 'title': 'A First Meetup' },
+        { 'title': 'A Second Meetup' }
+    ]
+    return render(req, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })

33888e2ae89a36244d888ee209244840f08b5c4b
amarchino <marchino.alessandro@gmail.com>
2026-05-31 21:31:18 +0200

    Lesson 241

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from .models import Meetup
+
 # Create your views here.
 
 def index(req: HttpRequest) -> HttpResponse:
-    meetups = [
-        { 'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup' },
-        { 'title': 'A Second Meetup', 'location': 'Paris', 'slug': 'a-second-meetup' }
-    ]
+    meetups = Meetup.objects.all()
     return render(req, 'meetups/index.html', {
-        'show_meetups': True,
         'meetups': meetups
     })
 
 def meetup_details(req: HttpRequest, meetup_slug: str) -> HttpResponse:
-    selected_meetup = { 'title': 'A First Meetup', 'description': 'This is the first meetup!' }
-    return render(req, 'meetups/meetup-details.html', {
-        'meetup': selected_meetup
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(req, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup': selected_meetup
+        })
+    except Exception as exc:
+        return render(req, 'meetups/meetup-details.html', {
+            'meetup_found': False
+        })

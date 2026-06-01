41c63c4c7b1e80f7b5bb2a24a9881dd2dd8d01b4
amarchino <marchino.alessandro@gmail.com>
2026-06-01 19:31:02 +0200

    Lesson 245

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ( 'title', 'slug' )
-    list_filter = ( 'title', )
+    list_filter = ( 'location', )
     prepopulated_fields = { 'slug': ( 'title', ) }
 
 # Register your models here.
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
